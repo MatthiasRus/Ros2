e6a4529ec7e14f5377331f603aba1996295c9e8b
MatthiasRus <matthiasworku611@gmail.com>
2026-04-03 08:29:17 +0400

    feat : Service and Parameter Config

--- src/roscpp/launch/servo.launch.py
+++ src/roscpp/launch/servo.launch.py
@@ -1,14 +1,17 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    path = os.path.join(get_package_share_directory("roscpp"), "config", "servo_param.yaml")
     return LaunchDescription([
         Node(
             package="roscpp",
             executable="ServoPublisherNode",
             namespace="arm",
             name="servo_publisher",
-            parameters=[{"channel": 1, "pulse": 1775}]
+            parameters=[path]
         ),
         Node(
             package="roscpp",
